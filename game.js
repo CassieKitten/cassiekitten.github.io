956bd4d83abc8cc87ecd1c6bddfdc933d99f82ef
Cassie Peele <powerfox1995@gmail.com>
2018-04-30 17:32:32 -0400

    Changed love formula, redesigned layout, added x-all buttons, changed date logic

--- game.js
+++ game.js
@@ -3,14 +3,14 @@ var tickRate = 10; // how many times per second the main game loop runs
 var kisses = 0;
 var happiness = 0;
 var money = 0;
-var love = 0;
 var girlfriends = 0;
+var love = 0;
 
 var workCost = 5; // how much happiness it costs to work
 var dateCost = 20; // how much money it costs to date
-var initialDateChance = .25; // chance to get a girlfriend from a date
-var dateChanceIncrement = .1 // increases date success chance after a failed date
-var dateChance = initialDateChance;
+var dateChance = .25; // chance to get a girlfriend from a date
+var badDates = 0; // how many consecutive dates without getting a girlfriend
+var maxBadDates = 3; // how many bad dates before automatically getting a girlfriend
 var giftCost = 30; // how much money it costs to give a gift
 var kissesPerGirlfriend = 1; // how many kisses each girlfriend gives per second
 var kissesPerSecond = 0; // combined kisses per second from all girlfriends
@@ -30,16 +30,16 @@ document.addEventListener('DOMContentLoaded', function () {
       money = parseFloat(localStorage.money);
       updateMoney();
     }
-    if (localStorage.love) {
-      love = parseFloat(localStorage.love);
-      updateLove();
-    }
     if (localStorage.girlfriends) {
       girlfriends = parseFloat(localStorage.girlfriends);
       updateGirlfriends();
     }
-    if (localStorage.dateChance) {
-      dateChance = parseFloat(localStorage.dateChance);
+    if (localStorage.love) {
+      love = parseFloat(localStorage.love);
+      updateLove();
+    }
+    if (localStorage.badDates) {
+      badDates = parseInt(localStorage.badDates);
     }
   } else {
     // No Web Storage support :(
@@ -53,20 +53,15 @@ function reset() {
   kisses = 0;
   happiness = 0;
   money = 0;
-  love = 0;
   girlfriends = 0;
+  love = 0;
   updateKisses();
   updateHappiness();
   updateMoney();
-  updateLove();
   updateGirlfriends();
-  resetDateChance();
-  localStorage.removeItem("kisses");
-  localStorage.removeItem("happiness");
-  localStorage.removeItem("money");
-  localStorage.removeItem("love");
-  localStorage.removeItem("girlfriends");
-  localStorage.removeItem("dateChance");
+  updateLove();
+  resetBadDates();
+  localStorage.clear();
 }
 
 function kiss(number) {
@@ -77,7 +72,10 @@ function kiss(number) {
 }
 
 function work(number) {
-  if (happiness >= (workCost * number)) {
+  if (happiness >= workCost) {
+    if (happiness < (workCost * number)) {
+      number = Math.floor(happiness / workCost);
+    }
     happiness -= (workCost * number);
     money += number;
     updateHappiness();
@@ -86,15 +84,22 @@ function work(number) {
 }
 
 function date(number) {
-  if (money >= (dateCost * number)) {
+  if (money >= dateCost) {
+    if (money < (dateCost * number)) {
+      number = Math.floor(money / dateCost);
+    }
     money -= (dateCost * number);
-    for (var i = 0; i < number; i++) {
-      if (Math.random() < dateChance || girlfriends <= 0) {
-        girlfriends++;
-        resetDateChance();
-      } else {
-        increaseDateChance();
+    if (number < 100) {
+      for (var i = 0; i < number; i++) {
+        if (Math.random() < dateChance || badDates >= maxBadDates) {
+          girlfriends++;
+          resetBadDates();
+        } else {
+          increaseBadDates();
+        }
       }
+    } else {
+      girlfriends += Math.round(number / getAverageDatesPerGirlfriend());
     }
     updateMoney();
     updateGirlfriends();
@@ -102,8 +107,17 @@ function date(number) {
   }
 }
 
+// We need to approximate this for the Date All button because Chrome crashes
+// if you try to generate 50 million random numbers in a row
+function getAverageDatesPerGirlfriend() {
+  return Math.min(1 / dateChance, maxBadDates + 1)
+}
+
 function gift(number) {
-  if (money >= (giftCost * number)) {
+  if (money >= giftCost) {
+    if (money < (giftCost * number)) {
+      number = Math.floor(money / giftCost);
+    }
     money -= (giftCost * number);
     love += number;
     updateMoney();
@@ -117,49 +131,49 @@ function round(value, decimals) {
   return Number(Number(Math.round(value + "e" + decimals) + "e-" + decimals).toFixed(decimals));
 }
 
-function increaseDateChance() {
-  dateChance += dateChanceIncrement;
-  localStorage.dateChance = dateChance;
+function increaseBadDates() {
+  badDates++;
+  localStorage.badDates = badDates;
 }
 
-function resetDateChance() {
-  dateChance = initialDateChance;
-  localStorage.dateChance = dateChance;
+function resetBadDates() {
+  badDates = 0;
+  localStorage.badDates = badDates;
 }
 
 function updateKisses() {
-  document.getElementById("kisses-display").innerHTML = "Kisses: " + round(kisses, 0).toLocaleString();
+  document.getElementById("kisses-stat").innerHTML = round(kisses, 0).toLocaleString();
   localStorage.kisses = kisses;
 }
 
 function updateHappiness() {
-  document.getElementById("happiness-display").innerHTML = "Happiness: " + round(happiness, 0).toLocaleString();
+  document.getElementById("happiness-stat").innerHTML = round(happiness, 0).toLocaleString();
   localStorage.happiness = happiness;
 }
 
 function updateMoney() {
-  document.getElementById("money-display").innerHTML = "Money: $" + round(money, 0).toLocaleString();
+  document.getElementById("money-stat").innerHTML = "$" + round(money, 0).toLocaleString();
   localStorage.money = money;
 }
 
 function updateLove() {
-  document.getElementById("love-display").innerHTML = "Love: " + round(love, 0).toLocaleString();
+  document.getElementById("love-stat").innerHTML = round(love, 0).toLocaleString();
   localStorage.love = love;
 }
 
 function updateGirlfriends() {
-  document.getElementById("girlfriends-display").innerHTML = "Girlfriends: " + round(girlfriends, 0).toLocaleString();
+  document.getElementById("girlfriends-stat").innerHTML = round(girlfriends, 0).toLocaleString();
   localStorage.girlfriends = girlfriends;
 }
 
 function updateKissesPerGirlfriend() {
-  kissesPerGirlfriend = 1 + (Math.pow(love, 4.0 / 3.0) / (love + 100));
-  document.getElementById("kisses-per-girlfriend-display").innerHTML = "Kisses per Girlfriend: " + round(kissesPerGirlfriend, 2).toLocaleString();
+  kissesPerGirlfriend = 1 + (Math.pow(love, 4.0 / 3.0) / (love + 50));
+  document.getElementById("kisses-per-girlfriend-stat").innerHTML = round(kissesPerGirlfriend, 2).toLocaleString();
 }
 
 function updateKissesPerSecond() {
   kissesPerSecond = girlfriends * kissesPerGirlfriend;
-  document.getElementById("kisses-per-second-display").innerHTML = "Kisses per Second: " + round(kissesPerSecond, 2).toLocaleString();
+  document.getElementById("kisses-per-second-stat").innerHTML = round(kissesPerSecond, 2).toLocaleString();
 }
 
 // Main game loop
