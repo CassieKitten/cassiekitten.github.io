228be405cbdbe5ab0ffba073612d589a67de6d70
Cassie Peele <powerfox1995@gmail.com>
2018-08-25 11:59:30 -0400

    Manually initialize modalElements array to try and fix iOS modal hiding

--- js/cassie.js
+++ js/cassie.js
@@ -3,8 +3,9 @@
 // http://www.opensource.org/licenses/mit-license.php
 
 var modal;
-var modalBodies;
-var modalHeads;
+// var modalBodies;
+// var modalHeads;
+var modalElements;
 
 document.addEventListener("DOMContentLoaded", function(event) {
   init();
@@ -13,8 +14,23 @@ document.addEventListener("DOMContentLoaded", function(event) {
 function init() {
   // Assign vars
   modal = document.getElementById("modal");
-  modalBodies = document.getElementsByClassName("modal-body");
-  modalHeads = document.getElementsByClassName("modal-head");
+  // modalBodies = document.getElementsByClassName("modal-body");
+  // modalHeads = document.getElementsByClassName("modal-head");
+  modalElements = [
+    document.getElementById("head-plat"),
+    document.getElementById("head-girl"),
+    document.getElementById("head-zero"),
+    document.getElementById("head-pong"),
+    document.getElementById("head-math"),
+    document.getElementById("head-pets"),
+    document.getElementById("info-plat"),
+    document.getElementById("info-girl"),
+    document.getElementById("info-zero"),
+    document.getElementById("info-pong"),
+    document.getElementById("info-math"),
+    document.getElementById("info-pets"),
+    modal
+  ];
 
   // Allow opening modal
   document.getElementById("row-plat").addEventListener("click", function() {openModal("plat");}, false);
@@ -40,27 +56,33 @@ function init() {
 }
 
 function openModal(game) {
-  modal.style.display = "block";
-  // document.getElementById("head-" + game).style.display = "block";
-  // document.getElementById("info-" + game).style.display = "block";
+  // modal.style.display = "block";
+  // // document.getElementById("head-" + game).style.display = "block";
+  // // document.getElementById("info-" + game).style.display = "block";
   var head = document.getElementById("head-" + game);
   var info = document.getElementById("info-" + game);
+  modal.classList.remove("u-hidden");
   head.classList.remove("u-hidden");
   info.classList.remove("u-hidden");
+  modal.hidden = false;
   head.hidden = false;
   info.hidden = false;
 }
 
 function closeModal() {
-  modal.style.display = "none";
-  for (var modalBody of modalBodies) {
-    // modalBody.style.display = "none";
-    modalBody.classList.add("u-hidden");
-    modalBody.hidden = true;
-  }
-  for (var modalHead of modalHeads) {
-    // modalHead.style.display = "none";
-    modalHead.classList.add("u-hidden");
-    modalHead.hidden = true;
+  // modal.style.display = "none";
+  // for (var modalBody of modalBodies) {
+  //   // modalBody.style.display = "none";
+  //   modalBody.classList.add("u-hidden");
+  //   modalBody.hidden = true;
+  // }
+  // for (var modalHead of modalHeads) {
+  //   // modalHead.style.display = "none";
+  //   modalHead.classList.add("u-hidden");
+  //   modalHead.hidden = true;
+  // }
+  for (element of modalElements) {
+    element.classList.add("u-hidden");
+    element.hidden = true;
   }
 }
